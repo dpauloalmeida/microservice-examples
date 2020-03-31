f1e6806b55aa15fd60a6816ec69f09bbb4365b9f
Danilo Almeida <idd_dalmeida@uolinc.com>
2020-03-31 18:31:00 -0300

    update gradle to subprojects

--- rental-car-example/build.gradle.kts
+++ rental-car-example/build.gradle.kts
@@ -1,38 +1,31 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.2.6.RELEASE"
-	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	kotlin("jvm") version "1.3.71"
-	kotlin("plugin.spring") version "1.3.71"
-	kotlin("plugin.jpa") version "1.3.71"
+	id("org.springframework.boot") version "2.2.6.RELEASE" apply false
+	id("io.spring.dependency-management") version "1.0.9.RELEASE" apply false
+	kotlin("jvm") version "1.3.71" apply false
+	kotlin("plugin.spring") version "1.3.71" apply false
+	kotlin("plugin.jpa") version "1.3.71" apply false
 }
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+subprojects {
+	apply(plugin = "io.spring.dependency-management")
 
-repositories {
-	mavenCentral()
-}
+	group = "com.example"
+	version = "0.0.1-SNAPSHOT"
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	repositories {
+		mavenCentral()
 	}
-}
 
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "1.8"
+		}
 	}
 }
