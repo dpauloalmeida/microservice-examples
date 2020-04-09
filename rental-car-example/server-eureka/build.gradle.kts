60768dc13d6d067da4085e2237d105d909d3bcc7
Danilo Almeida <idd_dalmeida@uolinc.com>
2020-04-09 00:00:42 -0300

    add eureka server

---
+++ rental-car-example/server-eureka/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+	id("org.springframework.boot")
+	kotlin("jvm")
+	kotlin("plugin.spring")
+}
+
+extra["springCloudVersion"] = "Hoxton.SR1"
+
+dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
--- rental-car-example/settings.gradle.kts
+++ rental-car-example/settings.gradle.kts
@@ -5,5 +5,6 @@ include(
     "service-inventory-command",
     "service-inventory-query",
     "service-emailer",
-    "service-insurance-partner-wrapper"
+    "service-insurance-partner-wrapper",
+    "server-eureka"
 )
