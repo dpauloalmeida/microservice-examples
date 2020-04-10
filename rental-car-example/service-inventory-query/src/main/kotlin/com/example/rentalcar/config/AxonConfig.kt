3321d690959100c2d9861cb867c3eefde9bbcb7b
Danilo Almeida <idd_dalmeida@uolinc.com>
2020-04-10 01:26:17 -0300

    update inventory query service

--- rental-car-example/service-inventory-query/src/main/kotlin/com/example/rentalcar/config/AxonConfig.kt
+++ rental-car-example/service-inventory-query/src/main/kotlin/com/example/rentalcar/config/AxonConfig.kt
@@ -1,14 +1,24 @@
 package com.example.rentalcar.config
 
 import org.axonframework.config.EventProcessingConfigurer
+import org.axonframework.eventhandling.tokenstore.inmemory.InMemoryTokenStore
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore
+import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine
 import org.axonframework.extensions.kafka.eventhandling.consumer.streamable.StreamableKafkaMessageSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ConditionalOnProperty(value = ["axon.kafka.consumer.event-processor-mode"], havingValue = "TRACKING")
 class AxonConfig {
+
+    @Bean
+    fun eventStore() = EmbeddedEventStore.builder().storageEngine(InMemoryEventStorageEngine()).build()
+
+    @Bean
+    fun tokenStore() = InMemoryTokenStore()
 
     @Autowired
     fun configureStreamableKafkaSource(processingConfigurer: EventProcessingConfigurer,
